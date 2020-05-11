d64ccb915b1646808a45222c510dc45436c3d397
Ksenia Kataeva <k.kataeva@psu.ru>
2020-05-12 01:08:49 +0500

    add page transitions

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,10 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
-    page.should_be_login_link()      # проверяем наличие ссылки
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
